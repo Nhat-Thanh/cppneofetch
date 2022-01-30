39c32dbbfc6c79f87876a41382dfbe21dca76e20
Nhat-Thanh <luonkebenban@gmail.com>
2022-01-30 10:34:05 +0700

    commit code

---
+++ logos.hpp
@@ -0,0 +1,79 @@
+#include <vector>
+#include <string>
+
+/* For custom logo */
+// std::vector<std::string> LOGO = {
+
+// };
+
+
+std::vector<std::string>  LOGO = {
+    "                     .%;888:8898898:              ",
+    "                   x;XxXB%89b8:b8%b88:            ",
+    "                .8Xxd                8X:.         ",
+    "              .8Xx;                    8x:.       ",
+    "            .tt8x          .d            x88;     ",
+    "         .@8x8;          .db:              xx@;   ",
+    "       ,tSXX°          .bbbbbbbbbbbbbbbbbbbB8x@;  ",
+    "     .SXxx            bBBBBBBBBBBBBBBBBBBBbSBX8;  ",
+    "   ,888S                                     pd!  ",
+    "  8X88/                                       q   ",
+    "  GBB.                                            ",
+    "   x%88        d888@8@X@X@X88X@@XX@@X@8@X.        ",
+    "     dxXd    dB8b8b8B8B08bB88b998888b88x.         ",
+    "      dxx8o                      .@@;.            ",
+    "        dx88                   .t@x.              ",
+    "          d:SS@8ba89aa67a853Sxxad.                ",
+    "            .d988999889889899dd.                  "
+};
+
+
+
+// These was stolen from paleofetch
+
+// std::vector<std::string> LOGO = {
+//     "                  -`                    ",
+//     "                 .o+`                   ",
+//     "                `ooo/                   ",
+//     "               `+oooo:                  ",
+//     "              `+oooooo:                 ",
+//     "              -+oooooo+:                ",
+//     "            `/:-:++oooo+:               ",
+//     "           `/++++/+++++++:              ",
+//     "          `/++++++++++++++:             ",
+//     "         `/+++ooooooooooooo/`           ",
+//     "        ./ooosssso++osssssso+`          ",
+//     "       .oossssso-````/ossssss+`         ",
+//     "      -osssssso.      :ssssssso.        ",
+//     "     :osssssss/        osssso+++.       ",
+//     "    /ossssssss/        +ssssooo/-       ",
+//     "  `/ossssso+/:-        -:/+osssso+-     ",
+//     " `+sso+:-`                 `.-/+oso:    ",
+//     "`++:.                           `-/+/   ",
+//     ".`                                 `/   "
+// };
+
+
+// 
+// made by Reddit user LnLcFlx2
+// std::vector<std::string>  LOGO = {
+// "                   ▄                     ",
+// "                  ▟█▙                    ",
+// "                 ▟███▙                   ",
+// "                ▟█████▙                  ",
+// "               ▟███████▙                 ",
+// "              ▂▔▀▜██████▙                ",
+// "             ▟██▅▂▝▜█████▙               ",
+// "            ▟█████████████▙              ",
+// "           ▟███████████████▙             ",
+// "          ▟█████████████████▙            ",
+// "         ▟███████████████████▙           ",
+// "        ▟█████████▛▀▀▜████████▙          ",
+// "       ▟████████▛      ▜███████▙         ",
+// "      ▟█████████        ████████▙        ",
+// "     ▟██████████        █████▆▅▄▃▂       ",
+// "    ▟██████████▛        ▜█████████▙      ",
+// "   ▟██████▀▀▀              ▀▀██████▙     ",
+// "  ▟███▀▘                       ▝▀███▙    ",
+// " ▟▛▀                               ▀▜▙   "
+// };
