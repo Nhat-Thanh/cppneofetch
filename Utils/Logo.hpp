e532acd0d133be1af478889a473ba4ff90492305
Nhat-Thanh <luonkebenban@gmail.com>
2026-01-03 14:33:41 +0700

    Remove redundant logo

--- Utils/Logo.hpp
+++ Utils/Logo.hpp
@@ -9,72 +9,32 @@
 
 // };
 
-std::vector<std::string> vecstr_Logo = {
-    " TTTTTTTTTTTT hhhh                                   hhhh         ",
-    " T::::::::::T h::h                                   h::h         ",
-    " T::::::::::T h::h                                   h::h         ",
-    " T:TT::::TT:T h::h                                   h::h         ",
-    " TT  T::T  TT h::h          aaaaaaaaa   nnn  nnnnnn  h::h         ",
-    "     T::T     h:::hhh       a::::::::a  n::nn::::::n h:::hhh      ",
-    "     T::T     h::::::hh     aaaaaaa:::a n::::::::::n h::::::hh    ",
-    "     T::T     h::::h:::h           a::a nn:::::::::n h::::h:::h   ",
-    "     T::T     h:::h h:::h    aaaaaa:::a  n:::nnn:::n h:::h h:::h  ",
-    "     T::T     h::h   h::h   a:::::::::a  n::n   n::n h::h   h::h  ",
-    "     T::T     h::h   h::h  a::aaaa::::a  n::n   n::n h::h   h::h  ",
-    "     T::T     h::h   h::h a::a    a:::a  n::n   n::n h::h   h::h  ",
-    "   TT::::TT   h::h   h::h a::a    a:::a  n::n   n::n h::h   h::h  ",
-    "   T::::::T   h::h   h::h a:::aaaa::::a  n::n   n::n h::h   h::h  ",
-    "   T::::::T   h::h   h::h  a::::::::a::a n::n   n::n h::h   h::h  ",
-    "   TTTTTTTT   hhhh   hhhh   aaaaaaaa aaa nnnn   nnnn hhhh   hhhh  "
-};
-
-// std::vector<std::string>  vecstr_Logo = {
-//     "                     .%;888:8898898:              ",
-//     "                   x;XxXB%89b8:b8%b88:            ",
-//     "                .8Xxd                8X:.         ",
-//     "              .8Xx;                    8x:.       ",
-//     "            .tt8x          .d            x88;     ",
-//     "         .@8x8;          .db:              xx@;   ",
-//     "       ,tSXX°          .bbbbbbbbbbbbbbbbbbbB8x@;  ",
-//     "     .SXxx            bBBBBBBBBBBBBBBBBBBBbSBX8;  ",
-//     "   ,888S                                     pd!  ",
-//     "  8X88/                                       q   ",
-//     "  GBB.                                            ",
-//     "   x%88        d888@8@X@X@X88X@@XX@@X@8@X.        ",
-//     "     dxXd    dB8b8b8B8B08bB88b998888b88x.         ",
-//     "      dxx8o                      .@@;.            ",
-//     "        dx88                   .t@x.              ",
-//     "          d:SS@8ba89aa67a853Sxxad.                ",
-//     "            .d988999889889899dd.                  "
-// };
-
 // Source: paleofetch
-
-// std::vector<std::string> vecstr_Logo = {
-//     "                  -`                    ",
-//     "                 .o+`                   ",
-//     "                `ooo/                   ",
-//     "               `+oooo:                  ",
-//     "              `+oooooo:                 ",
-//     "              -+oooooo+:                ",
-//     "            `/:-:++oooo+:               ",
-//     "           `/++++/+++++++:              ",
-//     "          `/++++++++++++++:             ",
-//     "         `/+++ooooooooooooo/`           ",
-//     "        ./ooosssso++osssssso+`          ",
-//     "       .oossssso-````/ossssss+`         ",
-//     "      -osssssso.      :ssssssso.        ",
-//     "     :osssssss/        osssso+++.       ",
-//     "    /ossssssss/        +ssssooo/-       ",
-//     "  `/ossssso+/:-        -:/+osssso+-     ",
-//     " `+sso+:-`                 `.-/+oso:    ",
-//     "`++:.                           `-/+/   ",
-//     ".`                                 `/   "
-// };
+std::vector<std::string> vecstr_Logo = {
+    "                  -`                    ",
+    "                 .o+`                   ",
+    "                `ooo/                   ",
+    "               `+oooo:                  ",
+    "              `+oooooo:                 ",
+    "              -+oooooo+:                ",
+    "            `/:-:++oooo+:               ",
+    "           `/++++/+++++++:              ",
+    "          `/++++++++++++++:             ",
+    "         `/+++ooooooooooooo/`           ",
+    "        ./ooosssso++osssssso+`          ",
+    "       .oossssso-````/ossssss+`         ",
+    "      -osssssso.      :ssssssso.        ",
+    "     :osssssss/        osssso+++.       ",
+    "    /ossssssss/        +ssssooo/-       ",
+    "  `/ossssso+/:-        -:/+osssso+-     ",
+    " `+sso+:-`                 `.-/+oso:    ",
+    "`++:.                           `-/+/   ",
+    ".`                                 `/   "
+};
 
 //
 // Made by Reddit user LnLcFlx2
-// std::vector<std::string>  vecstr_Logo = {
+// std::vector<std::string> vecstr_Logo = {
 // "                   ▄                     ",
 // "                  ▟█▙                    ",
 // "                 ▟███▙                   ",
